e40316bc39fde30bec29a1e4141c8ee7da2e078a
Janfrancisco <janramalho.100@gmail.com>
2024-10-31 20:11:20 -0400

    change the drop down menu effects

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,34 @@
 function scrollToElement(selector) {
   var el = document.querySelector(selector);
-  console.log("dbsblmçlml");
+
+  const body = document.body;
 
   el.scrollIntoView({ behavior: "smooth", block: "start" });
 
-  dropDownMenu.classList.remove("open");
-  toggleBtnIcon.classList = "fa-solid fa-bars";
+  const isOpenMenu = body.classList.contains("overflow-hidden");
+
+  if (isOpenMenu) {
+    body.classList.remove("overflow-hidden");
+  }
+
+  /*   dropDownMenu.classList.remove("open");
+  toggleBtnIcon.classList = "fa-solid fa-bars"; */
 }
 
 const toggleBtn = document.querySelector(".toggle-btn");
 const toggleBtnIcon = document.querySelector(".toggle-btn i");
 const dropDownMenu = document.querySelector(".dropdown-menu");
+const coverSection = document.querySelector(".cover");
+const body = document.body;
 
-toggleBtn.onclick = function () {
+function toogleDropDown() {
   // const isClosed = dropDownMenu.contains("hidden");
   dropDownMenu.classList.toggle("open");
+  coverSection.classList.toggle("animate-blur");
 
   const isOpen = dropDownMenu.classList.contains("open");
+  body.classList.toggle("overflow-hidden");
+  toggleBtn.classList.toggle("hidden");
 
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-};
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     fontFamily: {
       sans: "Ubuntu",
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        blur: {
+          "0%": { filter: "blur(0px)" },
+          "100%": { filter: "blur(4px)" },
+        },
+      },
+      animation: {
+        blur: "blur 0.5s linear forwards",
+      },
+    },
   },
   plugins: [],
 };
